4d56571a7f555353c86eedbcc7cbaa3e2a4c55aa
Vamsi Krishna Reddy <vamsi3@iitb.ac.in>
2020-06-20 08:49:10 +0530

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+particlesJS.load('particles-js', 'assets/particles.json', function() {});
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+$(document).ready(function() {
+    var colors = ['#8064a2', '#5bc0de', 'black', '#d9534f', 'green', 'orange'];
+    var buttons = ['btn-outline-dark', 'btn-outline-info', 'btn-outline-dark', 'btn-outline-danger', 'btn-outline-success', 'btn-outline-warning'];
+    var idx = Math.floor(Math.random() * colors.length);
+    var col = colors[idx],
+        btn = buttons[idx];
+    $('#particles-js').css('background-color', col);
+    $('a[role="button"]').addClass(btn);
+    $('a[class="dropdown-item"]').addClass(btn);
+    $('button').addClass(btn);
+});
